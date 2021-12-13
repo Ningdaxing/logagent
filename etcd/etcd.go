e38b6ac9e883ef46fac70961fb74c55ee7981e96
ning.daxing <ning.daxing@immomo.com>
2021-12-13 10:52:46 +0800

    fix

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -12,12 +12,12 @@ import (
 )
 
 var (
-	client  *clientv3.Client
+	client *clientv3.Client
 )
 
 func Init(address []string) (err error) {
 	client, err = clientv3.New(clientv3.Config{
-		Endpoints: address,
+		Endpoints:   address,
 		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
@@ -29,12 +29,12 @@ func Init(address []string) (err error) {
 }
 
 // GetConf 拉取日志收集配置项
-func GetConf(key string) (collectEntryList []common.CollectEntry, err error)  {
+func GetConf(key string) (collectEntryList []common.CollectEntry, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
 	resp, err := client.Get(ctx, key)
-	if err != nil{
+	if err != nil {
 		logrus.Errorf("faild to get key:%s, err:%v", key, err)
 		return
 	}
@@ -44,7 +44,7 @@ func GetConf(key string) (collectEntryList []common.CollectEntry, err error)  {
 	}
 	ret := resp.Kvs[0]
 	err = json.Unmarshal(ret.Value, &collectEntryList)
-	if err != nil{
+	if err != nil {
 		logrus.Errorf("json unmarshal faild, err:%v\n", err)
 		return
 	}
@@ -53,19 +53,19 @@ func GetConf(key string) (collectEntryList []common.CollectEntry, err error)  {
 }
 
 // WatchConf 监控etcd中日志收集配置变化的函数
-func WatchConf(key string)  {
+func WatchConf(key string) {
 	watchCh := client.Watch(context.Background(), key)
 	var newConf []common.CollectEntry
-	for wresp := range watchCh{
-		for _, evt := range wresp.Events{
+	for resp := range watchCh {
+		for _, evt := range resp.Events {
 			fmt.Printf("type: %s, key: %s, values: %s\n", evt.Type, evt.Kv.Key, evt.Kv.Value)
 			err := json.Unmarshal(evt.Kv.Value, &newConf)
-			if err != nil{
+			if err != nil {
 				logrus.Errorf("json unmarshl new conf faild, err:%v", err)
 				continue
 			}
 			// 告诉tailfile模块应该启动新的配置了
-			tailfile.SendNewConf(newConf)  // 没有任何接收就是阻塞的
+			tailfile.SendNewConf(newConf) // 没有任何接收就是阻塞的
 		}
 	}
 }
